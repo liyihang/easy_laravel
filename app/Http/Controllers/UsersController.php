cb1f96cd4bb81c350bb4458a598793a415af0c19
liyihang <lidoudoou@gmail.com>
2018-08-20 16:35:35 +0800

    pagination

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -33,7 +33,7 @@ class UsersController extends Controller
 
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(10);
         return view('users.index',compact('users'));
     }
 
